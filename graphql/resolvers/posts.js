3773acd7ff97dabb54ff81cc54d824c134eeeb22
Daniel Unapanta <dunapanta2@gmail.com>
2020-10-23 11:27:36 -0500

    Post query

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -8,7 +8,19 @@ module.exports = {
                 const posts = await Post.find();
                 return posts;
             }catch(err){
-                throw new Error(error);
+                throw new Error(err);
+            }
+        },
+        async getPost(parent, { postId } ){
+            try{
+                const post = await Post.findById(postId);
+                if(post){
+                    return post;
+                } else {
+                    throw new Error('Post not found')
+                }
+            } catch(err){
+                throw new Error(err)
             }
         }
     }
