bb4632b97e2dacf0ffbe3639b5df8a5c843196f5
Daniel Unapanta <dunapanta2@gmail.com>
2020-11-20 15:17:14 -0600

    config

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://daniel:ac2EBNFU5nNWgh8h@social-media-app.ljvva.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'aplicacion merng'
+}
+
